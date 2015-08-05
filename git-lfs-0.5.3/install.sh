5c803d38439bcdb6264310d836c08f5049b05d31
Mark Muchane <muchanem@gmail.com>
2015-08-04 20:23:51 -0400

    save

---
+++ git-lfs-0.5.3/install.sh
@@ -0,0 +1,19 @@
+#!/bin/sh -eu
+
+prefix="/usr/local"
+
+if [ "${PREFIX:-}" != "" ] ; then
+  prefix=${PREFIX:-}
+elif [ "${BOXEN_HOME:-}" != "" ] ; then
+  prefix=${BOXEN_HOME:-}
+fi
+
+mkdir -p $prefix/bin
+
+rm -rf $prefix/bin/git-lfs*
+for g in git*; do
+  install $g "$prefix/bin/$g"
+done
+
+PATH+=:$prefix/bin
+git lfs init
