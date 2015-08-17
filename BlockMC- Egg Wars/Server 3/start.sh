0c865d6752549378be8457d91a536ad108fbb5b0
Mark Muchane <muchanem@gmail.com>
2015-08-16 20:13:09 -0400

    save

---
+++ BlockMC- Egg Wars/Server 3/start.sh
@@ -0,0 +1,18 @@
+{\rtf1\ansi\ansicpg1252\cocoartf1348\cocoasubrtf170
+{\fonttbl\f0\fnil\fcharset0 Menlo-Regular;}
+{\colortbl;\red255\green255\blue255;\red109\green109\blue109;\red234\green234\blue234;\red0\green0\blue0;
+\red37\green127\blue159;\red43\green39\blue19;}
+\margl1440\margr1440\vieww10800\viewh8400\viewkind0
+\deftab720
+\pard\pardeftab720
+
+\f0\fs26 \cf2 \cb3 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec2 #!/bin/bash\cf4 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec4 \
+java -\cf5 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec5 Xms10M\cf4 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec4  -\cf5 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec5 Xmx1G\cf4 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec4  -jar spigot1649.jar\cf6 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec6 \
+}
